cd88905a553d99c4977d1a95f9c98ca361a7a266
iqbalbaharum <balainkk@gmail.com>
2018-02-21 19:33:30 +0800

    update css links

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    path = require('path'),
     ejs = require('ejs');
 
 const port = 8080;
@@ -9,7 +10,7 @@ var app = express();
 // Body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname + "/public")));
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
